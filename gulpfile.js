80ee71ce3779466ee71b58d2b3dda9a298abe778
macbook <diego92ms@gmail.com>
2017-10-13 12:02:29 -0300

    Atualizando Curriculo

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,24 @@
-var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
- 
-gulp.task('sass', function () {
-  return sass('scss/style.scss')
-  .pipe(gulp.dest('css/'))
-});	
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    rename = require('gulp-rename'),
+    uglifycss = require('gulp-uglifycss');
+
+//Processa main.css
+gulp.task('sass:main', function () {
+    sass('scss/main.scss').on('error', sass.logError)
+    .pipe(uglifycss({
+      "uglyComments": true
+    }))
+    .pipe(rename("main.min.css"))
+    .pipe(gulp.dest('./'));
+
+});
+
+gulp.task('default', function () {
+    gulp.watch([
+        'scss/*/*.scss',
+        'scss/*.scss'
+    ], [
+        'sass:main'
+    ]);
+});
